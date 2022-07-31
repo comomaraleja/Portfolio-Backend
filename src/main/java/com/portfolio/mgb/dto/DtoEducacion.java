d9391156fc4dfdae8fc6d126cdbfef6484373ffa
Alejandra Correa <comomaraleja@gmail.com>
2022-07-31 12:02:04 -0300

    make it better

--- src/main/java/com/portfolio/mgb/dto/DtoEducacion.java
+++ src/main/java/com/portfolio/mgb/dto/DtoEducacion.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolio.mgb.dto;
 
 import javax.validation.constraints.NotBlank;
 
-/**
- *
- * @author NADA
- */
 public class DtoEducacion {
     
      @NotBlank
--- src/main/java/com/portfolio/mgb/service/SEducacion.java
+++ src/main/java/com/portfolio/mgb/service/SEducacion.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolio.mgb.service;
 
 import com.portfolio.mgb.entity.Educacion;
@@ -12,10 +9,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- *
- * @author NADA
- */
+
 @Service
 @Transactional
 public class SEducacion {
