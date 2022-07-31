37b6cf4be90b7e7180627d55ba7b2c59030fc4a8
Alejandra Correa <comomaraleja@gmail.com>
2022-07-31 18:15:35 -0300

    segundo intento deploy

--- src/main/java/com/portfolio/mgb/controller/TimeController.java
+++ src/main/java/com/portfolio/mgb/controller/TimeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1.0")
+@RequestMapping("/api/v1.0")
 public class TimeController {
     @GetMapping("/time")
     @ResponseStatus(HttpStatus.OK)
