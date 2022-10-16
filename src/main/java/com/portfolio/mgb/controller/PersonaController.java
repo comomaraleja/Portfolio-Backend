38e462060ff1c4f13f32989bd020e17590e52370
Alejandra Correa <comomaraleja@gmail.com>
2022-10-16 02:00:29 -0300

    cambios

--- src/main/java/com/portfolio/mgb/controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/controller/PersonaController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+/*import org.springframework.web.bind.annotation.RequestMapping;*/
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/personas")
+/*@RequestMapping("/personas")*/
 @CrossOrigin(origins = "https://frontend-ap-7eeda.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
